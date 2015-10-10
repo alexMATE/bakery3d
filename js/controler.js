06295ab5b08e451109482f6aca4fbf827bbbc5f7
alexMATE <sweetflavordisign@gmail.com>
2015-10-10 14:42:07 +0100

    Subiendo Todos los archivos

---
+++ js/controler.js
@@ -0,0 +1,10 @@
+wow = new WOW(
+    {
+      boxClass:     'wow',      // default
+      animateClass: 'animated', // default
+      offset:       0,          // default
+      mobile:       true,       
+      live:         true        // default
+    }
+  )
+wow.init();
